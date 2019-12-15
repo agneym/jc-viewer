b83ab2c6d620c4ecdb4efca10c29c86293eaf591
Agney <agney@outlook.in>
2019-12-15 15:53:53 +0530

    allow common js imports

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,19 @@
 import { createDefaultConfig } from '@open-wc/building-rollup';
 
+import commonjs from 'rollup-plugin-commonjs';
+
 // if you need to support IE11 use `createCompatibilityConfig` instead.
 // import { createCompatibilityConfig } from '@open-wc/building-rollup';
 // export default createCompatibilityConfig({ input: './index.html' });
 
-export default createDefaultConfig({ input: './index.html' });
+const config = createDefaultConfig({
+  input: './index.html',
+  plugins: {
+    workbox: false,
+  },
+});
+
+export default {
+  ...config,
+  plugins: [...config.plugins, commonjs()],
+};
