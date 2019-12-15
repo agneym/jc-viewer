14b5897f4cb1eb415f5b4849953c964e1f8b91ff
Agney <agney@outlook.in>
2019-12-15 14:32:40 +0530

    correct testing

--- test/jc-viewer.test.js
+++ test/jc-viewer.test.js
@@ -1,11 +1,11 @@
 import { html, fixture, expect } from '@open-wc/testing';
 
-import '../jc-json-utils.js';
+import '../jc-viewer.js';
 
-describe('JcJsonUtils', () => {
+describe('JcViewer', () => {
   it('has a default title "Hey there" and counter 5', async () => {
     const el = await fixture(html`
-      <og-json-utils></og-json-utils>
+      <jc-viewer></jc-viewer>
     `);
 
     expect(el.data).to.equal(undefined);
